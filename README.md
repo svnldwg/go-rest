cadc1bc29452e89e9fb1b264968bd91f80da452a
svnldwg <s.ludwig.g@gmail.com>
2020-12-16 23:56:33 +0100

    sets up first API endpoint

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## Run
 
+Start web server:
 `go run main.go`
+
+Access web server:
+http://localhost:10000/
--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"encoding/json"
     "fmt"
     "log"
     "net/http"
 )
+
+type Dish struct {
+    Title string `json:"title"`
+    Desc string `json:"desc"`
+}
+
+// let's declare a global array
+// that we can then populate in our main function
+// to simulate a database
+var Dishes []Dish
+
+func returnAllDishes(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Endpoint Hit: returnAllDishes")
+    json.NewEncoder(w).Encode(Dishes)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -12,10 +28,16 @@ func homePage(w http.ResponseWriter, r *http.Request){
 }
 
 func handleRequests() {
-    http.HandleFunc("/", homePage)
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/dishes", returnAllDishes)
     log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func main() {
+	Dishes = []Dish{
+        Dish{Title: "Tacos", Desc: "Maistacos, Hackfleisch, Tomaten, Guacamole"},
+        Dish{Title: "Tamales", Desc: "Auch lecker"},
+	}
+	
     handleRequests()
 }
